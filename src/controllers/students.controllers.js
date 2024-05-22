82d13b900f651249fe4af3aa6e8cf7d43619391f
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-05-22 12:32:23 +0800

    feat: add api and test for creating students

--- src/controllers/students.controllers.js
+++ src/controllers/students.controllers.js
@@ -9,49 +9,49 @@ import {
 export const listStudents = async (req, res) => {
 	try {
 		const resp = await queryListStudents();
-		res.status(200).json(resp)
+		res.status(200).json(resp);
 
 	} catch (err) {
-		res.status(500).send(err)
+		res.status(500).send(err);
 	}
 }
 
 export const createStudent = (req, res) => {
 	try {
 		const resp = queryCreateStudent(req.body);
-		res.status(200).json(resp)
+		res.status(200).json(resp);
 
 	} catch (err) {
-		res.status(500).send(err)
+		res.status(500).send(err);
 	}
 }
 
 export const getStudent = (req, res) => {
 	try {
-		const resp = queryGetStudent(parseInt(req.params.id))
-		res.status(200).json(resp)
+		const resp = queryGetStudent(parseInt(req.params.id));
+		res.status(200).json(resp);
 
 	} catch (err) {
-		res.status(500).send(err)
+		res.status(500).send(err);
 	}
 }
 
 export const updateStudent = (req, res) => {
 	try {
-		const resp = queryUpdateStudent(parseInt(req.params.id), req.body)
-		res.status(200).json(resp)
+		const resp = queryUpdateStudent(parseInt(req.params.id), req.body);
+		res.status(200).json(resp);
 
 	} catch (err) {
-		res.status(500).send(err)
+		res.status(500).send(err);
 	}
 }
 
 export const deleteStudent = (req, res) => {
 	try {
-		const resp = queryDeleteStudent(parseInt(req.params.id))
-		res.status(200).json(resp)
+		const resp = queryDeleteStudent(parseInt(req.params.id));
+		res.status(200).json(resp);
 
 	} catch (err) {
-		res.status(500).send(err)
+		res.status(500).send(err);
 	}
 }
