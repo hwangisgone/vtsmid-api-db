65579c22643bc6bb22667dba6546e6551c9ebf80
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-05-26 22:56:10 +0800

    bugfix: containers not connecting

--- runcontainers.sh
+++ runcontainers.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-docker run --name vts-db --env-file=prod.env -p 5432:5432 -d vts-image-db-postgres
-docker run --name vts-api --env-file=prod.env -p 3000:3000 -d vts-image-api
+docker network inspect vts_mid_network >/dev/null 2>&1 || \
+    docker network create --driver bridge vts_mid_network
+
+docker run --name vts-db --network=vts_mid_network --env-file=prod.env -p 5433:5432 -d vts-image-db-postgres
+docker run --name vts-api --network=vts_mid_network --env-file=prod.env -p 3000:3000 -d vts-image-api
