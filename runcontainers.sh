bd479ebb56973707144cc8adcb416bc6e553d03d
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-05-26 23:11:54 +0800

    bugfix: change env variable
    Environmental variable changed to work with postgres container and shell script

--- runcontainers.sh
+++ runcontainers.sh
@@ -3,5 +3,7 @@
 docker network inspect vts_mid_network >/dev/null 2>&1 || \
     docker network create --driver bridge vts_mid_network
 
-docker run --name vts-db --network=vts_mid_network --env-file=prod.env -p 5433:5432 -d vts-image-db-postgres
+docker volume create pgdata
+
+docker run --name vts-db --network=vts_mid_network --env-file=prod.env -p 5433:5432 -v pgdata:/var/lib/postgresql/data -d vts-image-db-postgres
 docker run --name vts-api --network=vts_mid_network --env-file=prod.env -p 3000:3000 -d vts-image-api
--- tests/postgres-client.mjs
+++ tests/postgres-client.mjs
@@ -10,7 +10,7 @@ const pgclient = new Client({
 	port: process.env.POSTGRES_PORT,
 	user: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
-	database: process.env.POSTGRES_DATABASE,
+	database: process.env.POSTGRES_DB,
 });
 
 await pgclient.connect();
